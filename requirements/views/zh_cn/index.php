5552d4087de4ca0b3cbcf5b92562faedf19d473f
creturn <master@creturn.com>
2014-01-06 12:22:19 +0800

    webapp测试

--- requirements/views/zh_cn/index.php
+++ requirements/views/zh_cn/index.php
@@ -3,18 +3,19 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <meta http-equiv="content-language" content="en"/>
-<link rel="stylesheet" type="text/css" href="css/main.css" />
+<!-- <link rel="stylesheet" type="text/css" href="css/main.css" /> -->
+<link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" />
 <title>Yii配置需求检查</title>
 </head>
 
 <body>
-<div id="page">
+<div class="page">
 
-<div id="header">
+<div class="header">
 <h1>Yii配置需求检查</h1>
 </div><!-- header-->
 
-<div id="content">
+<div id="content" class="bs-header">
 <h2>检查内容</h2>
 <p>
 本网页用于确认您的服务器配置是否能满足运行<a href="http://www.yiiframework.com/">Yii</a> Web应用的要求。它将检查服务器所运行的PHP版本，查看是否安装了合适的PHP扩展模块，以及确认php.ini文件是否正确设置。
@@ -33,14 +34,14 @@
 
 <h2>具体结果</h2>
 
-<table class="result">
+<table class="table table-bordered table-responsive">
 <tr><th>项目名称</th><th>结果</th><th>使用者</th><th>备注</th></tr>
 <?php foreach($requirements as $requirement): ?>
 <tr>
 	<td>
 	<?php echo $requirement[0]; ?>
 	</td>
-	<td class="<?php echo $requirement[2] ? 'passed' : ($requirement[1] ? 'failed' : 'warning'); ?>">
+	<td class="<?php echo $requirement[2] ? 'success' : ($requirement[1] ? 'failed' : 'warning'); ?>">
 	<?php echo $requirement[2] ? '通过' : '未通过'; ?>
 	</td>
 	<td>
